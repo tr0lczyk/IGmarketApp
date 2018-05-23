9b45a643329f6ef87a5b6822db643700ee4256bc
tr0lczyk <matt.olczyk@gmail.com>
2018-05-23 22:51:29 +0200

    added image forlder for pngs, readme tbc

--- app/src/main/java/com/example/android/igmarketapp/MainActivity.java
+++ app/src/main/java/com/example/android/igmarketapp/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     public static final String LOG = MainActivity.class.getName();
 
